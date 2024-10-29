492f963828c0e631bb2ac3d33d9306b95b235776
IMAD-Majid <imadvlp0@gmail.com>
2024-10-29 16:43:56 +0000

    fixed resizing

--- main.js
+++ main.js
@@ -1,21 +1,14 @@
 const gifPath = {
     "Push-ups": "gif/push-up.gif",
     Squats: "gif/squat.gif",
-    // Burpees: "gif/burpee.gif",
-    // Lunges: "gif/lunges.gif",
-    // Plank: "png/plank.png",
 };
+const core = ["Push-ups", "Squats"];
 
+// 30, 46, 12, 30 = 30 minutes
 const totalExercises = 30;
 const exerciseDur = 46;
 const restDur = 12;
 const roundDur = 30;
-
-// CORE
-
-const core = ["Push-ups", "Squats"];
-
-// CORE
 
 let completed = 0;
 let exercises = [];
@@ -27,15 +20,13 @@ for (let i = 0; i < totalExercises; i++) {
 exercises.sort(() => Math.random() - 0.5);
 
 // DOM
-const workoutElm = document.getElementById("workout-ui");
+const signElm = document.getElementById("sign");
+const workoutElm = document.querySelector("main");
 const exerciseNameElm = document.getElementById("name");
 const exerciseGifElm = document.getElementById("workout-gif");
 const timerElm = document.getElementById("timer");
 const progressElm = document.getElementById("progress");
-const reportElms = document.querySelectorAll(
-    "#progress-container, #progress-container *"
-);
-const supportLink = document.querySelector("a");
+const progressDiv = document.querySelector("#progress-container");
 const btn = document.querySelector("button");
 
 btn.addEventListener("click", start);
@@ -43,15 +34,21 @@ btn.addEventListener("click", start);
 function start() {
     workoutElm.classList.toggle("hidden");
     progressElm.textContent = totalExercises;
-    btn.style.display = "none";
+    btn.classList.add("hidden");
+    document.getElementById("trainer").classList.add("hidden");
+    progressDiv.classList.remove("hidden");
     nextExercise();
+
+    resizeGif();
 }
 function nextExercise() {
     let exerciseName = exercises[completed];
 
     workoutElm.className = "exercise";
     exerciseNameElm.textContent = exerciseName;
+
     exerciseGifElm.src = gifPath[exerciseName];
+
     exerciseGifElm.classList.toggle("hidden");
 
     let duration = exerciseDur;
@@ -63,21 +60,16 @@ function nextExercise() {
             clearInterval(timer);
             completed++;
             progressElm.textContent = totalExercises - completed;
-            if (completed % 10) {
-                exerciseRest();
+            if (completed < totalExercises) {
+                if (completed % 10) exerciseRest();
+                else roundRest();
             } else {
-                if (completed < totalExercises) {
-                    roundRest();
-                } else {
-                    reportElms.forEach((elm) => {
-                        elm.style.backgroundColor = "yellow";
-                        elm.style.color = "black";
-                    });
+                progressDiv.classList.add("completed");
+                progressElm.textContent = "trainning completed";
 
-                    workoutElm.classList.toggle("hidden");
-                    exerciseGifElm.classList.toggle("hidden");
-                    supportLink.style.display = "flex";
-                }
+                workoutElm.classList.toggle("hidden");
+                exerciseGifElm.classList.toggle("hidden");
+                signElm.classList.toggle("hidden");
             }
         }
     }, 1000);
@@ -93,7 +85,7 @@ function exerciseRest() {
         duration--;
         timerElm.textContent = duration;
         if (duration <= 10 && completed < totalExercises) {
-            timerElm.textContent += " (" + exercises[completed] + ")";
+            timerElm.textContent += " >>> " + exercises[completed];
         }
         if (duration <= 0) {
             clearInterval(timer);
@@ -121,3 +113,15 @@ function roundRest() {
         }
     }, 1000);
 }
+
+function resizeGif() {
+    // default is 320 px
+    // shrink when viewport height is scrollable
+    // resize by decreasing the difference of excess from 320
+    const excess = document.body.scrollHeight - document.body.clientHeight;
+    exerciseGifElm.style.maxWidth = (320 - excess) + "px";
+    exerciseGifElm.style.height = "auto";
+}
+
+// listen to window resizing
+window.addEventListener("resize", resizeGif);
--- sw.js
+++ sw.js
@@ -1,49 +1,71 @@
-const CACHE_VERSION = "2024-08-26 1:10 PM";
+const CACHE_VERSION = "2024-10-29 16:37";
+localStorage.setItem("CACHE_VERSION", CACHE_VERSION)
+
+// cache files list
+const cf = [
+    "/",
+	"/font.css",
+	"/style.css",
 
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_VERSION).then((cache) => {
-      return cache.addAll([
-          "/",
-          "/index.html",
-          "/style.css",
-          "/main.js",
-          "/icon.png",
-          "/manifest.json",
-          "/sw.js",
+	"gif/push-up.gif",
+	"gif/squat.gif",
 
-          "/png/plank.png",
-          "/png/power pose.png",
-          "/png/rest.png",
-          "/png/round.png",
+	"/index.html",
 
-          "/gif/squat.gif",
-          "/gif/push-up.gif",
-      ]);
-    })
-  );
-});
+	"/main.js",
+	"/sw.js",
 
+	"/icon.png",
+	"png/power pose.png",
+	"png/rest.png",
+	"png/round.png",
 
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheName !== CACHE_VERSION) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
-});
-
-
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
+	"fonts/RobotoMono-Bold.ttf",
+	"fonts/RobotoMono-Italic.ttf",
+	"fonts/RobotoMono-Light.ttf",
+
+];
+self.addEventListener("install", (event) => {
+    event.waitUntil(
+        caches.open(CACHE_VERSION).then(async (cache) => {
+            console.log("ServiceWorker: Caching files:", cf.length, cf);
+            try {
+                cachedResult = await cache.addAll(cf);
+            } catch (err) {
+                console.error("sw: cache.addAll");
+                for (let f of cf) {
+                    try {
+                        cachedResult = await cache.add(f);
+                    } catch (err) {
+                        console.warn("sw: cache.add", f);
+                    }
+                }
+            }
+            console.log("ServiceWorker: caching ended");
+
+            return cachedResult;
+        })
+    );
+});
+
+self.addEventListener("activate", (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (cacheName !== CACHE_VERSION) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
+
+self.addEventListener("fetch", (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
 });
