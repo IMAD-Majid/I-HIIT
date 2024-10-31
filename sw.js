ec429b23de89f65997cb8c412e57f98c16e274be
IMAD-Majid <imadvlp0@gmail.com>
2024-10-31 01:35:01 +0000

    fixed PWA

--- sw.js
+++ sw.js
@@ -1,71 +1,74 @@
-const CACHE_VERSION = "2024-10-29 16:37";
-localStorage.setItem("CACHE_VERSION", CACHE_VERSION)
-
-// cache files list
-const cf = [
-    "/",
+const CACHE_VERSION = "2024-10-31 01:34";
+localStorage.setItem("CACHE_VERSION", CACHE_VERSION)
+
+// cache files list
+const cf = [
+    "/",
 	"/font.css",
 	"/style.css",
 
-	"gif/push-up.gif",
-	"gif/squat.gif",
+	"/gif/push-up.gif",
+	"/gif/squat.gif",
 
 	"/index.html",
 
 	"/main.js",
+	"/register-sw.js",
 	"/sw.js",
 
 	"/icon.png",
-	"png/power pose.png",
-	"png/rest.png",
-	"png/round.png",
+	"/png/power pose.png",
+	"/png/rest.png",
+	"/png/round.png",
 
-	"fonts/RobotoMono-Bold.ttf",
-	"fonts/RobotoMono-Italic.ttf",
-	"fonts/RobotoMono-Light.ttf",
-
-];
-self.addEventListener("install", (event) => {
-    event.waitUntil(
-        caches.open(CACHE_VERSION).then(async (cache) => {
-            console.log("ServiceWorker: Caching files:", cf.length, cf);
-            try {
-                cachedResult = await cache.addAll(cf);
-            } catch (err) {
-                console.error("sw: cache.addAll");
-                for (let f of cf) {
-                    try {
-                        cachedResult = await cache.add(f);
-                    } catch (err) {
-                        console.warn("sw: cache.add", f);
-                    }
-                }
-            }
-            console.log("ServiceWorker: caching ended");
-
-            return cachedResult;
-        })
-    );
-});
-
-self.addEventListener("activate", (event) => {
-    event.waitUntil(
-        caches.keys().then((cacheNames) => {
-            return Promise.all(
-                cacheNames.map((cacheName) => {
-                    if (cacheName !== CACHE_VERSION) {
-                        return caches.delete(cacheName);
-                    }
-                })
-            );
-        })
-    );
-});
-
-self.addEventListener("fetch", (event) => {
-    event.respondWith(
-        caches.match(event.request).then((response) => {
-            return response || fetch(event.request);
-        })
-    );
+	"/fonts/RobotoMono-Bold.ttf",
+	"/fonts/RobotoMono-Italic.ttf",
+	"/fonts/RobotoMono-Light.ttf",
+
+	"/app.webmanifest",
+
+];
+self.addEventListener("install", (event) => {
+    event.waitUntil(
+        caches.open(CACHE_VERSION).then(async (cache) => {
+            console.log("ServiceWorker: Caching files:", cf.length, cf);
+            try {
+                cachedResult = await cache.addAll(cf);
+            } catch (err) {
+                console.error("sw: cache.addAll");
+                for (let f of cf) {
+                    try {
+                        cachedResult = await cache.add(f);
+                    } catch (err) {
+                        console.warn("sw: cache.add", f);
+                    }
+                }
+            }
+            console.log("ServiceWorker: caching ended");
+
+            return cachedResult;
+        })
+    );
+});
+
+self.addEventListener("activate", (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (cacheName !== CACHE_VERSION) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
+
+self.addEventListener("fetch", (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
 });
